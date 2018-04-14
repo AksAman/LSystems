f223d6e44c97099bf99fa38b6d5e9ce5521dae59
AksAman <krsingh.aman@gmail.com>
2018-04-15 05:03:25 +0530

    Added LSytems.py

--- StringGenerator.py
+++ StringGenerator.py
@@ -2,31 +2,27 @@ rules={'A':'AB','B':'A'}
 
 import sys
 
-a='A'
-b='B'
-
 axiom='A'
 sentence=''
 
 
 if(len(sys.argv) is 1):
-	generation=int(input('Enter the number of generations:'))
+	generations=int(input('Enter the number of generations:'))
 else:
-	generation=int(sys.argv[1])
+	generations=int(sys.argv[1])
 
 
-def generate(_sent,_gen,_rules):
-	_sent=axiom
+def generate(_axiom,_gens,_rules,_sent):
+	_sent=_axiom
 	string=''
 	rulesList=list(_rules)
 
 	print('Generation 0 : '+axiom)
-	for i in range(_gen):
+	for i in range(_gens):
 		for char in _sent:
 			for rule in range(len(_rules)):
 				if char is rulesList[rule]:
 					string+=rules[rulesList[rule]]
-
 		_sent=string
 		string=''
 		print('Generation '+str(i+1)+' : '+_sent)
@@ -34,5 +30,5 @@ def generate(_sent,_gen,_rules):
 
 
 if __name__=="__main__":
-	LString=generate(sentence,generation,rules)
+	LString=generate(axiom,generations,rules,sentence)
 	print("Final "+LString)
