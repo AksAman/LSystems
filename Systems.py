32fb5f82a59d1bbd0128c73550ef4bf0d07dd2ca
AksAman <krsingh.aman@gmail.com>
2018-04-15 07:53:04 +0530

    edited systems as dict

--- Systems.py
+++ Systems.py
@@ -1,30 +1,39 @@
-Koch Curve
-rules={'F':'F+F-F-F+F'}
-axiom='F'
+{
+	'Koch Curve':{
+				'rules':{'F':'F+F-F-F+F'},
+				'axiom':'F'
+	},
 
-Plant
-rules={'X':'F+[[X]-X]-F[-FX]+X','F':'FF'}
-axiom='X'
+	'Plant':{
+				'rules':{'X':'F+[[X]-X]-F[-FX]+X','F':'FF'}
+				'axiom':'X'
+	},
 
-Fractal Tree
-rules={'F':'FF','0':'F[-0]+0'}
-axiom='0'
+	'Fractal Tree':{
+				'rules':{'F':'FF','0':'F[-0]+0'}
+				'axiom':'0'
+	},
 
-Cantor Set
-axiom='A'
-rules={'A':'AfA','f':'fff'}
+	'Cantor Set':{
+				'axiom':'A'
+				'rules':{'A':'AfA','f':'fff'}
+	},
 
-Islands and lakes
-axiom='F+F+F+F'
-rules={'F':'F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF','f':'ffffff'}
+	'Islands and lakes':{
+				'axiom':'F+F+F+F'
+				'rules':{'F':'F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF','f':'ffffff'}
+	},
 
-FASS Curves
-axiom='A'
-rules={'A':'A+B++B-A--AA-B+','B':'-A+BB++B+A--A-B'}
-
-Plant B (25.7)
-axiom='F'
-rules={'F':'F[+F]F[-F]F'}
-
-axiom='F'
-rules={'F':'FF-[-F+F+F]+[+F-F-F]'}
+	'FASS Curves':{
+				'axiom':'A'
+				'rules':{'A':'A+B++B-A--AA-B+','B':'-A+BB++B+A--A-B'}
+	},
+	'Plant B':{
+				'axiom':'F'
+				'rules':{'F':'F[+F]F[-F]F'}
+	},
+	'Plant C':{
+				'axiom':'F'
+				'rules':{'F':'FF-[-F+F+F]+[+F-F-F]'}
+	},
+}
